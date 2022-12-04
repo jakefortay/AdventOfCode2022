aed3dd6989a261cbda0685097d43f3d23d11829c
Jake Fortay <jakefortay@gmail.com>
2022-12-04 15:41:38 -0500

    Done Day 1

--- Day01/AdventDay1.cpp
+++ Day01/AdventDay1.cpp
@@ -3,20 +3,23 @@
 #include <fstream> 
 #include <iostream>
 #include <string> 
+#include <vector> 
 
 using namespace std; 
 
 int main()
 {
+
+    // runningSum is the answer to part 1, topThreeTotal is the answer to part 2
+
     ifstream newFile("text.txt");
     string input; 
     string testInput;
     int tempMax = 0; 
-    int runningSum = 0; 
+    int runningSum = 0;
+    vector<int> groupedValues;  
 
     while(getline(newFile,input)){
-        
-        cout << input << "cash" << endl;  
         
         if(input != ""){
             runningSum += stoi(input);
@@ -24,12 +27,38 @@ int main()
             if(runningSum > tempMax){
                 tempMax = runningSum; 
             }
+            groupedValues.push_back(runningSum);
             runningSum = 0;
         }
     }
     
-    cout << tempMax; 
-    
+    vector<int> topThree; 
+    int localMax = 0; 
+    int counter = 0; 
+
+    for(int j = 0; j < 3; j++){
+        for(int i = 0; i < groupedValues.size(); i++){
+            if(groupedValues[i] > localMax){
+                localMax = groupedValues[i]; 
+                counter = i;
+            }
+        }
+        topThree.push_back(localMax);
+        groupedValues.erase(groupedValues.begin() + counter);
+        localMax = 0; 
+    }
+
+    int topThreeTotal = 0; 
+
+    for(int i = 0; i < topThree.size(); i++){
+        cout << topThree[i] << endl; 
+        topThreeTotal += topThree[i];
+    }
+
+    cout << topThreeTotal; 
+
+
     return 0;
+
 }
 
--- README.md
+++ README.md
@@ -3,12 +3,19 @@
 This is the journey, of how one man, one stellar man, took the trek that others were unwilling to go. To complete all 25 days of the advent of code 2022. 
 
 ---
-To Run Thine Code
+To Run Thine Code -
 
 ---
 
+Ex. Using Day 1 - (Inside of Day 1 Folder)
+
 ```
+PS C:\Users\Jake\Desktop\AdventOfCode2022\Day01>
+g++ -o Day1.exe AdventDay1.cpp
+
+Previous error was running this command - 
 g++ -o Day1.exe ./Day01/AdventDay1.cpp
+Here we were already in the Day01 folder, then tried to navigate to a Day01 folder within that, which did not exist. 
 
 ./Day1.exe
 ```
