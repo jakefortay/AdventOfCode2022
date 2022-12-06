bcbe0f7c37fc5d93c6dd132f5397dc97a5d656a6
Jake Fortay <jakefortay@gmail.com>
2022-12-06 01:05:00 -0500

    Done day 6

---
+++ Day06/AdventDay6.cpp
@@ -0,0 +1,69 @@
+
+#include <stdio.h>
+#include <fstream> 
+#include <iostream>
+#include <string> 
+#include <vector> 
+
+using namespace std; 
+
+bool testChars(vector<char> vec, int position){
+    bool flag = true;
+    int numChars = 14; 
+    vector<char> newVec; 
+
+    for(int i = 0; i < numChars; i++){
+        newVec.push_back(vec[position + i]);
+    }
+
+    int iterator = 0; 
+    int temp = numChars; 
+
+    for(int k = 0; k < numChars; k++){
+        for(int j = 1; j < temp; j++){
+            if(newVec[iterator] == newVec[iterator + j]){
+                flag = false; 
+            }
+        }
+        iterator += 1; 
+        temp -= 1; 
+    }
+
+    return flag; 
+
+}
+
+
+int main()
+{
+
+    ifstream newFile("text_6.txt");
+
+    char input; 
+
+    int count, index(22); 
+
+    vector<char> vec; 
+    bool test,foundIndex(0); 
+
+    while(newFile >> input){
+        vec.push_back(input);
+    }
+
+
+    for(int i = 0; i < vec.size() - 5; i++){
+        test = testChars(vec,i);
+        if(test == true && foundIndex == false){
+            index = i + 14; 
+            foundIndex = true; 
+        }
+    }
+
+    // cout << vec[index] << " " << vec[index + 1] << " " << vec[index + 2] << " " << vec[index + 3] << endl; 
+
+    cout << index; 
+
+    return 0;
+
+}
+
