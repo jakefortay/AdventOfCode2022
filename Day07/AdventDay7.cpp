2573971592bfb4fe5febda86cd268f39e0ee65ff
Jake Fortay <jakefortay@gmail.com>
2022-12-10 23:57:28 -0500

    Finished Day 8

---
+++ Day07/AdventDay7.cpp
@@ -0,0 +1,57 @@
+
+#include <stdio.h>
+#include <fstream> 
+#include <iostream>
+#include <string> 
+#include <vector> 
+
+using namespace std; 
+
+const int MAX_SIZE = 100000;
+
+int getSize(vector<string> list, vector<string> directories, vector<int> positions){
+
+    for(int i = 0; i < positions.size(); i++){
+
+    }
+
+
+
+
+    return 0; 
+}
+
+
+int main(){
+
+    fstream newFile("text_7.txt");
+    string input, subLine, subString;
+    vector<string> data; 
+    vector<string> directories; 
+    vector<int> positions; 
+
+    while(getline(newFile, input)){
+        data.push_back(input);
+    }
+
+    for(int i = 1; i < data.size(); i++){
+        subLine = data.at(i);
+
+        if(subLine.at(0) == '$' && subLine.at(2) == 'c' && subLine.at(5) != '.'){
+            subString = subLine.substr(4,subLine.size() - 4);
+            directories.push_back(subString);
+            positions.push_back(i);
+        }
+        
+    }
+
+    for(int i = 0; i < directories.size(); i++){
+        cout << directories.at(i) << " " << positions.at(i) << endl; 
+    }
+
+
+
+    return 0;
+
+}
+
---
+++ Day08/AdventDay8.cpp
@@ -0,0 +1,188 @@
+
+#include <stdio.h>
+#include <fstream> 
+#include <iostream>
+#include <string> 
+#include <vector> 
+
+using namespace std; 
+
+int main(){
+
+    ifstream newFile("text_8.txt");
+    ifstream firstLine("text_8.txt");
+
+    string first; 
+    getline(firstLine,first);
+
+    const int rowSize = first.size(); 
+
+    char input; 
+    int temp; 
+    
+
+    vector<vector<int>> grid;
+    vector<int> tempVect = {};
+
+    for(int i = 0; i < rowSize; i++){
+        grid.push_back(tempVect);
+    }
+
+    int i = 0; 
+
+    while(newFile >> input){
+
+        temp = int(input) - 48;
+        grid[i].push_back(temp);
+        i++;
+
+        if(i == rowSize){
+            i = 0; 
+        }
+
+
+    }
+
+    int treesVisible = 0; 
+    int pos; 
+    bool isVisNorth, isVisSouth, isVisEast, isVisWest; 
+    int scenicScore; 
+    int v1, v2, v3, v4, tempPos, runCount; 
+    vector<int> scenicScores; 
+
+    for(int i = 1; i < grid.size() - 1; i++){
+        for(int j = 1; j < grid[i].size() - 1; j++){
+            isVisNorth = true; 
+            isVisSouth = true; 
+            isVisEast = true;
+            isVisWest = true; 
+
+            bool endNow = false; 
+
+            v1 = 0;
+            v2 = 0;
+            v3 = 0; 
+            v4 = 0; 
+
+
+
+            // cout << "Coord is : " << i << " " << j << " and equals " << grid[i][j] << endl; 
+
+            // Checks upwards
+
+            pos = j - 1;
+            tempPos = pos; 
+
+            while(pos >= 0){
+                if(grid[i][pos] >= grid[i][j]){
+                    pos = 0; 
+                    isVisNorth = false; 
+                }
+                if(grid[i][tempPos] >= grid[i][j]){  
+                    v1 = 1;
+                }
+                else{
+                    v1++; 
+                }
+                pos--; 
+            }
+            
+            // Checks downwards 
+
+            pos = j + 1;
+            tempPos = pos; 
+
+            while(pos <= grid[i].size() - 1){
+                if(grid[i][pos] >= grid[i][j]){
+                    pos = grid[i].size() + 1; 
+                    isVisSouth = false; 
+                }
+                if(grid[i][tempPos] >= grid[i][j]){  
+                    v2 = 1;
+                }
+                else{
+                    v2++; 
+                }
+                pos++;
+            }
+            
+            // Checks to the left
+
+            pos = i - 1;
+            tempPos = pos; 
+
+            while(pos >= 0){
+                if(grid[pos][j] >= grid[i][j]){
+                    pos = 0; 
+                    isVisWest = false; 
+                }
+                if(grid[tempPos][j] >= grid[i][j]){  
+                    v3 = 1;
+                }
+                else{
+                    v3++; 
+                }
+
+                pos--; 
+            }
+
+
+            // Checks to the right 
+
+            pos = i + 1; 
+            tempPos = pos; 
+
+            while(pos <= grid.size() - 1){
+                if(grid[pos][j] >= grid[i][j]){
+                    pos = grid.size() + 1; 
+                    isVisEast = false; 
+                }
+                // cout << "Got here" << endl; 
+                if(grid[tempPos][j] >= grid[i][j]){  
+                    // cout << "Is " << grid[tempPos][j] << " greater than or equal to " << grid[i][j] << endl; 
+                    v4 = 1;
+                }
+                else{
+                    v4++; 
+                }
+  
+                pos++; 
+            }
+
+
+        
+
+            if(isVisNorth || isVisSouth || isVisEast || isVisWest){
+                treesVisible++; 
+            }
+
+
+            // cout << "Scores are : " << v1 << "*" << v2 << "*" << v3 << "*" << v4 << endl;
+            // cout << "Total is : " << v1 * v2 * v3 * v4 << endl << endl; 
+
+            scenicScore = v1 * v2 * v3 * v4; 
+
+            scenicScores.push_back(scenicScore);
+        
+        }
+    }
+
+    int edgesVisible = (grid.size() * 2) + (grid[0].size() * 2) - 4; 
+    cout << edgesVisible << " " << treesVisible << endl; 
+    int totalTrees = edgesVisible + treesVisible; 
+    cout << endl << "Total trees is : " << totalTrees << endl; 
+
+    int tempMax = scenicScores[0];
+
+    for(int i = 0; i < scenicScores.size(); i++){
+        if(scenicScores[i] >= tempMax){
+            tempMax = scenicScores[i];
+        }
+    }
+
+    cout << "Max scenic score is " << tempMax << endl; 
+
+    return 0;
+
+}
+
